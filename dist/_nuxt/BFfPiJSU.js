df3b6947e41922dc91b64bde39ede91b9e0a2ba7
unknown <olyachubash@gmail.com>
2026-07-09 19:57:56 +0200

    delete dist from git ignore

---
+++ dist/_nuxt/BFfPiJSU.js
@@ -0,0 +1,1 @@
+import{f as o,j as t,e as r}from"./C3yfpvp3.js";const l={__name:"BasicFooter",setup(s){return(a,e)=>(r(),o("section",null,e[0]||(e[0]=[t("h1",null,"Footer",-1)])))}};export{l as default};
